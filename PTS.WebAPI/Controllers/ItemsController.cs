c43bf6966b3e232e7d9d07be2941a253cdee0594
INTRAEDGEPUNE\cbhattacharyya <cbhattacharyya@intraedge.com>
2017-03-02 13:27:59 +0530

    Items stub added

---
+++ PTS.WebAPI/Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PTS.WebAPI.Models;
+using Swashbuckle.Swagger.Annotations;
+
+namespace PTS.WebAPI.Controllers
+{
+    [RoutePrefix("pts/Items")]
+    public class ItemsController : ApiController
+    {
+        // GET: pts/Items/{id} - Get a specific item by ID
+        /// <summary>
+        /// Get a specific item by ID
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns>Item</returns>
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(ItemResponse))]
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new ItemResponse(true, false).Items);
+        }
+
+        // GET: pts/items/waybill/{id} - Get waybill given the Item ID
+        [HttpGet]
+        [Route("waybill/{id}")]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(ItemResponse))]
+        public HttpResponseMessage GetByItem(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new ItemResponse(true, true).Items);
+        }
+    }
+}
---
+++ PTS.WebAPI/Models/ItemResponse.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PTS.Business.Domain;
+
+namespace PTS.WebAPI.Models
+{
+    public class ItemResponse : IModelBase
+    {
+        public int Id { get; set; }
+
+        public int Waybill { get; set; }
+
+        public int Truck { get; set; }
+
+        public string Facility { get; set; }
+
+        public string Location { get; set; }
+
+        public bool IsDamaged { get; set; }
+
+        public bool IsOSD { get; set; }
+
+        public string Notes { get; set; }
+
+        public string Desc { get; set; }
+
+        public Damage Damage { get; set; }
+
+        public string Who { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public List<Item> Items { get; set; }
+
+        public ItemResponse(bool fillDummyData, bool isWaybill)
+        {
+            if (!isWaybill)
+            {
+                Items = new List<Item>
+                {
+                    new Item()
+                    {
+                        Id = 1112223333,
+                        Waybill = 1122334455,
+                        Truck = 1,
+                        Facility = "UT1003",
+                        Location = "Aisle 3",
+                        IsDamaged = false,
+                        IsOSD = false,
+                        Notes = "",
+                        Desc = "Futura Dining room chair",
+                        Damage = new Damage { Id = 1, Image = "", IsActive = true, Notes = "", Date = DateTime.Now },
+                        Who = "",
+                        IsActive = true
+                    }
+                };
+             }
+
+            else if (isWaybill)
+            {
+                Items = new List<Item>
+                {
+                   new Item()
+                    {
+                        Id = 1112223333,
+                        Waybill = 1122334455,
+                        Facility = "UT1003",
+                        Location = "Aisle 3",
+                        IsDamaged = false,
+                        IsOSD = false,
+                        Desc = "Futura Dining room chair"
+                    },
+                    new Item()
+                    {
+                        Id = 1112223334,
+                        Waybill = 1122334455,
+                        Facility = "UT1003",
+                        Location = "Aisle 3",
+                        IsDamaged = false,
+                        IsOSD = false,
+                        Desc = "Futura Dining room chair"
+                    },
+                    new Item()
+                    {
+                        Id = 1112223335,
+                        Waybill = 1122334455,
+                        Facility = "UT1003",
+                        Location = "Aisle 3",
+                        IsDamaged = false,
+                        IsOSD = false,
+                        Desc = "Futura Dining room chair"
+                    }
+               };
+            }
+        }
+    }
+}
